9af32ec141e559d41f735ed8ff722d7934875980
Kalel-Guedes <kalelcmg2006@gmail.com>
2025-06-27 16:51:37 -0300

    Add Animations
    adicionando animações para o player e para o inimigo

--- Assets/Scripts/PrototipoMovimento/PlayerMovement.cs
+++ Assets/Scripts/PrototipoMovimento/PlayerMovement.cs
@@ -10,6 +10,11 @@ public class PlayerMovement : MonoBehaviour
     public float speedRun;
     public float _currentSpeed;
 
+    public string animationTrigger = "Walk";
+    public string animationJump = "Jump";
+    public Animator animator;
+    
+
 
     public float forcejump;
 
@@ -25,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.linearVelocity = Vector2.up * forcejump;
+            animator.SetBool(animationJump, true);
+        }
+        else
+        {
+           animator.SetBool(animationJump, false); 
         }
     }
     private void Movement()
@@ -32,19 +42,31 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             myRigidbody.linearVelocity = new Vector2(-_currentSpeed, myRigidbody.linearVelocity.y);
+            myRigidbody.transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetBool(animationTrigger, true);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             myRigidbody.linearVelocity = new Vector2(_currentSpeed, myRigidbody.linearVelocity.y);
+            myRigidbody.transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool(animationTrigger, true);
         }
+        else
+        {
+           animator.SetBool(animationTrigger, false); 
+        }
+
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
             _currentSpeed = speedRun;
+            animator.speed = 1.5f;
+
         }
         else
         {
             _currentSpeed = speed;
+            animator.speed = 1f;
         }
     }
 }
